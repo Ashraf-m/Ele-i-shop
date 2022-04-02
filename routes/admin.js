2a01b954745bf7ffef8c559118ab13df25418b41
Ashraf-m <ashrafm0113@gmail.com>
2022-04-02 13:38:39 +0530

    cart page setuped

---
+++ routes/admin.js
@@ -0,0 +1,63 @@
+var express = require('express');
+const productHelpers = require('../helpers/product-helpers');
+var router = express.Router();
+var productHelper=require('../helpers/product-helpers')
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+
+  res.render('admin/dashboard',{admin:true})
+});
+
+router.get("/product-manager", (req, res,next) => {
+    productHelpers.getAllProducts().then((products)=>{
+      console.log(products);
+      res.render("admin/product-manager", { admin: true, products});
+    })
+   
+  });
+
+  router.get("/add-product", (req, res) => {
+ 
+    res.render("admin/add-product", { admin: true, });
+  });
+
+
+ 
+    
+  
+
+  router.post('/add-product',(req,res)=>{
+    //console.log(req.body);
+   
+    let image1=req.files.image1
+    let image2=req.files.image2
+    let image3=req.files.image3
+    let image4=req.files.image4
+
+
+    productHelper.addProduct(req.body,(id)=>{
+      console.log(id);
+      
+    image1.mv("./public/product-images/" + id + "first" + ".jpg");
+    image2.mv("./public/product-images/" + id + "second" + ".jpg");
+    image3.mv("./public/product-images/" + id + "third" + ".jpg");
+      image4.mv('./public/product-images/'+id+ "fourth" + ".jpg",(err,done)=>{
+        if(!err){
+          res.redirect("/admin/product-manager")
+        }else{
+          console.log(err);
+        }
+      })
+     
+    })
+  })
+
+
+ 
+
+
+
+
+  
+
+module.exports = router;
---
+++ routes/user.js
@@ -0,0 +1,79 @@
+const { response } = require("express");
+var express = require("express");
+var router = express.Router();
+var productHelper = require("../helpers/product-helpers");
+const userHelpers=require('../helpers/user-helpers')
+
+const verifyLogin=(req,res,next)=>{
+  if(req.session.loggedIn){
+    next()
+  }else{
+    res.redirect('/login')
+  }
+}
+
+/* GET home page. */
+router.get("/",  function (req, res, next) {
+  let userDetails=req.session.user
+  console.log(userDetails);
+   productHelper.getAllProducts().then((products) => {
+    res.render("index", { products, userDetails, user:true });
+  });
+});
+
+
+router.get("/login",(req,res,next)=>{
+  if(req.session.loggedIn){
+    res.redirect('/')
+  }else{
+  res.render("user/login",{"loginErr":req.session.loginErr})
+  req.session.loginErr=false
+  }
+  })
+
+
+router.get("/signup",(req,res,next)=>{
+  res.render("user/signup")
+  })
+router.post('/signup',(req,res)=>{
+   userHelpers.doSignup(req.body).then((response)=>{
+    console.log(response);
+    res.redirect('/')
+  })
+})
+
+router.post('/login',(req,res)=>{
+  userHelpers.doLogin(req.body).then((response)=>{
+    if(response.status){
+      req.session.loggedIn=true
+      req.session.user=response.user
+      res.redirect('/')
+    }else{
+      req.session.loginErr="Invalid username or Password"
+      res.redirect('/login')
+    }
+  })
+})
+
+router.get('/logout',(req,res)=>{
+  req.session.destroy()
+  res.redirect('/')
+})
+
+router.get("/cart",verifyLogin,(req,res,next)=>{
+  var userDetails=req.session.user
+  console.log(userDetails);
+  
+res.render("user/cart",{userDetails,user:true})
+})
+
+router.get("/checkout",(req,res)=>{
+console.log("check");
+var userDetails=req.session.user
+  console.log(userDetails);
+res.render("user/checkout",{userDetails,user:true})
+})
+
+
+
+module.exports = router;
